c151df23a0b64dea57f44936969df4cdf60068a2
Harshit-gc <harshitgargacharya@gmail.com>
2025-12-30 12:31:12 +0530

    [Harshit Gargacharya] Refactor.Modified Use case Handling

--- core-java-practice/scenario-based/EmployeeWageComputation.java
+++ core-java-practice/scenario-based/EmployeeWageComputation.java
@@ -1,51 +1,39 @@
 package scenario_based;
-import java.util.*;
 
 public class EmployeeWageComputation {
 	
-	public static boolean presenceStatus() {
-		int status = (int)(Math.random()*10)%3;
-		if(status == 0) {
-			return false;
-		}else {
-			return true;
-		}
-	}
+	// UC 2 & 3: Daily Employee Wage, Add Part Time Employee
+	// UC 5: Assuming 20 Working Days in a month
+	public static final int PART_TIME = 1;
+	public static final int FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int MAX_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.println("...Welcome to Employee Wage Computation Program...");
-		// Use Case 3 and 4 Part Time Employee and Wage Using Switch Case
-		System.out.println("Employee Type");
-		System.out.println("1. Part Time");
-		System.out.println("2. Full Time");
-		System.out.println("Enter Choice 1 or 2");
-		int choice = input.nextInt();
-		String employeeType = "";
-		switch(choice) {
-		case 1:
-			employeeType = "Part Time";
-			break;
-		case 2:
-			employeeType = "Full Time";
-			break;
-		default:
-			System.out.println("Invalid Input");
-		}
-		System.out.println("Employee Type: "+employeeType);
-		int totalEmpHours = 0;
-		int totalDays = 0;
-		// Use Case 6 Calculate Wages till a condition reaches for a month.
-		while(totalEmpHours<=100 && totalDays <= 20) {
-			// UseCase 1 Check Employee is Present or Absent.
-			if(presenceStatus()) {
-				totalEmpHours += 8;
-				totalDays += 1;
-			}
-		}
-		// Use Case 5 & 2 Calculate Wages for a month.
-		int monthlyWage = totalEmpHours*20;
-		System.out.print(monthlyWage);
-		input.close();
+	    int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+	    
+	    // UC 6: Calculate Wages till max working hours and max days is reached.
+	    while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < MAX_WORKING_DAYS) {
+	        totalWorkingDays++;
+	        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+	        
+	        // UC 1: Check Employee is Absent Or Present.
+	        // UC 4: Solving Using Switch Case
+	        switch (empCheck) {
+	            case PART_TIME:
+	                empHrs = 4;
+	                break;
+	            case FULL_TIME:
+	                empHrs = 8;
+	                break;
+	            default:
+	                empHrs = 0;
+	        }
+	        totalEmpHrs += empHrs;
+	        System.out.println("Day: " + totalWorkingDays + " Emp Working Hour: " + empHrs);
+	    }
+	    int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+	    System.out.println("Total Employee Wage: " + totalEmpWage);
 	}
 }
